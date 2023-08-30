e0386e3fae499f196fade54b55bc0f5f5660c1df
Neocon Technologies LTD <neocontechltd@gmail.com>
2023-08-30 13:11:52 +0600

    add why choose us section

--- components/Homepage/WhyChoose.js
+++ components/Homepage/WhyChoose.js
@@ -11,10 +11,10 @@ function WhyChoose() {
   return (
     <>
       <div className="py-5">
-        <div className="flex justify-between w-full">
-          <div className="w-1/2">
+        <div className="flex justify-between xsm:flex-col sm:flex-col w-full">
+          <div className="w-1/2 xsm:w-full sm:w-full">
             <div className="bg-gradient-to-tr from-[#DEF7F2] to-[#C0ECD7] rounded-r-md">
-              <div className="flex flex-col py-20 px-10 pl-40">
+              <div className="flex flex-col py-20 px-10 pl-40 xsm:px-5  xsm:py-10 sm:px-5  sm:py-10">
                 <p className="text-4xl text-lap_black font-medium pb-5">
                   We've <br />
                   earned lot of <br />
@@ -43,10 +43,13 @@ function WhyChoose() {
               </div>
             </div>
           </div>
-          <div className="px-5"></div>
+          <div className="px-5 xsm:hidden sm:hidden block"></div>
+          <div className="hidden xsm:block sm:block">
+            <Image src={vector} alt="vector.png" className="w-auto h-auto mx-auto" />
+          </div>
           <div className="w-full">
             <div className="bg-gradient-to-r from-[#DEF7F2] to-[#C0ECD7] rounded-l-md">
-              <div className="flex flex-col pt-12 pb-20 px-10 pl-40">
+              <div className="flex flex-col pt-12 pb-20 px-10 pl-40 xsm:px-5  xsm:py-10 sm:px-5  sm:py-10">
                 <p className="text-4xl text-lap_black font-medium pb-6">
                   Why Everybody Choose <br />
                   <span className="text-lap_blue font-extrabold">
@@ -99,7 +102,7 @@ function WhyChoose() {
             </div>
           </div>
         </div>
-        <div className="relative">
+        <div className="relative xsm:hidden sm:hidden block">
           <div className="absolute -top-80 -mt-32 left-80 w-5/12">
             <Image src={vector} alt="vector.png" className=" h-auto" />
           </div>
